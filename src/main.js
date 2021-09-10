f6c0a4332f5988e7ccc2b2d448e8b75ac2f949fe
imamiya-masaki <imamiyagit@gmail.com>
2021-09-11 03:14:18 +0900

    routingの下地を作る

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import Simple from './Simple.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+const routes = [
+  { path: '/a', component: Simple }
+]
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+createApp(App).use(router).mount('#app')
