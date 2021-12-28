ba6cb494a223d75cfe7d4d5cc23ac0d749f37b93
Emiri <e.coma.125@gmail.com>
2021-12-28 18:42:23 +0900

    webdesignのページ作成

---
+++ src/components/Webdesign.vue
@@ -0,0 +1,43 @@
+<template>
+    <section>
+      <img :src="imgsrc" width="990" alt="" />
+      <h1>{</h1>
+      <pre>
+<slot/>
+      </pre>
+      <button v-on:click="togithub">git hub</button>
+    </section>
+</template>
+
+<script>
+export default {
+  name: 'Webdesign',
+  components: {},
+  props: ['src', 'imgsrc', 'name'],
+  data: function () {
+    return {}
+  },
+  methods: {
+    togithub: function () {
+      window.location = this.src
+    }
+  }
+}
+
+</script>
+<style scoped>
+article {
+    width: auto;
+    max-width: 1600px;
+    font-family: '游ゴシック';
+}
+section {
+    width: 100%;
+    max-width: 990px;
+    margin: 0 auto;
+}
+pre {
+    font-family: '游ゴシック';
+    margin: 0;
+}
+</style>
---
+++ src/mitame/Webdesign.vue
@@ -0,0 +1,59 @@
+<template>
+  <article>
+    <section>
+      <img src="../assets/Photos/Sonota/watabiwa-img.jpg" width="990" alt="" />
+      <h1>ワタシノビワコ フォトコンテスト</h1>
+      <pre>
+自主制作として、滋賀県の琵琶湖をメインとしたフォトコンテストの特設サイトを作成しました。
+初めてのwebサイト構築であり、レスポンシブ対応にとても苦戦しました。
+私の地元である滋賀県の琵琶湖と、趣味である写真撮影を組み合わせたテーマにしました。
+写真が好きなあらゆる年代の方に向けてシンプルでわかりやすいデザインを心掛け、また手軽に参加していただけるように
+スマホ・一眼レフ・フィルムカメラのどんなカメラでも参加できるコンテストにしました。
+      </pre>
+      <button v-on:click="towatabiwa">git hub</button>
+    </section>
+    <section>
+      <img src="../assets/Photos/Sonota/e-coma-img.jpg" width="990" alt="" />
+      <h1>E-COMA PORTFOLIO.SITE</h1>
+      <pre>
+私自身のポートフォリオサイトを作成しました。
+      </pre>
+      <button v-on:click="toecoma">git hub</button>
+    </section>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'Webdesign',
+  components: {},
+  data: function () {
+    return {}
+  },
+  methods: {
+    towatabiwa: function () {
+      window.location = 'https://github.com/E-coma/photocon'
+    },
+    toecoma: function () {
+      window.location = 'https://github.com/E-coma/e-coma'
+    }
+  }
+}
+</script>
+<style scoped>
+article {
+  width: auto;
+  max-width: 1600px;
+  font-family: "游ゴシック";
+  margin: 0 auto;
+}
+section {
+  width: 100%;
+  max-width: 990px;
+  margin: 0 auto;
+}
+pre {
+  font-family: "游ゴシック";
+  margin: 0;
+}
+</style>
