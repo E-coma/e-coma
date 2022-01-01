f694f3ba17bd85e5f692331d04223e7316ed5aaa
Emiri <e.coma.125@gmail.com>
2022-01-02 00:50:24 +0900

    Webdesignページのgithubボタン修正

--- src/mitame/Webdesign.vue
+++ src/mitame/Webdesign.vue
@@ -2,7 +2,10 @@
   <article>
     <section class="watabiwa">
       <img src="../assets/Photos/watabiwa-img.jpg" width="990" alt="" />
+      <div class="titlegroup">
       <h1>ワタシノビワコ フォトコンテスト</h1>
+      <button v-on:click="towatabiwa">GIT HUB</button>
+      </div>
       <pre>
 自主制作として、滋賀県の琵琶湖をメインとしたフォトコンテストの特設サイトを作成しました。
 初めてのwebサイト構築であり、レスポンシブ対応にとても苦戦しました。
@@ -11,15 +14,17 @@
 写真が好きなあらゆる年代の方に向けてシンプルでわかりやすいデザインを心掛け、また手軽に参加していただけるように
 スマホ・一眼レフ・フィルムカメラのどんなカメラでも参加できるコンテストにしました。
       </pre>
-      <button v-on:click="towatabiwa">git hub</button>
+
     </section>
     <section class="ecoma">
       <img src="../assets/Photos/e-coma-img.jpg" width="990" alt="" />
+      <div class="titlegroup">
       <h1>E-COMA PORTFOLIO.SITE</h1>
+      <button v-on:click="toecoma">GIT HUB</button>
+      </div>
       <pre>
 私自身のポートフォリオサイトを作成しました。
       </pre>
-      <button v-on:click="toecoma">git hub</button>
     </section>
   </article>
 </template>
@@ -54,8 +59,20 @@ section {
   margin: 57px auto 112px auto;
 
 }
+h1 {
+  margin: 0;
+  margin-right: 10px;
+}
+.titlegroup {
+  display: flex;
+}
 pre {
   font-family: "游ゴシック";
   margin: 0;
+}
+button {
+font-size: 13px;
+margin-bottom: 5px;
+margin-top: 3px;
 }
 </style>
